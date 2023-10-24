453287d403a1669bea9cedda77f418bceb06fd7e
Mohammed Zaieda <m.zaieda@gmail.com>
2023-10-24 16:34:43 +0300

    get min in the second branch

--- Task 1/task1.cpp
+++ Task 1/task1.cpp
@@ -11,8 +11,19 @@ int getSum(vector<int> arr) {
     return sum;
 }
 
+int getMin(vector<int> arr) {
+    int min = arr[0];
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
 int main() {
     vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8,  9, 10 };
     cout << getSum(arr) << endl;
+    cout << getMin(arr) << endl;
     return 0;
 }
